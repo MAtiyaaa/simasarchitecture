b20ca8475a16a15fd44f9ce7c3528a509605417d
Atiya <matiyaaaa@gmail.com>
2025-04-18 02:17:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -449,7 +449,7 @@ const pageNavigation = {
             aboutLink.classList.remove('active');
             pdfContainer.classList.remove('active');
             aboutContainer.classList.remove('active');
-            window.location.href = 'mailto:sima.assaf@outlook.com';
+            window.location.href = 'mailto:simasarchitecture@gmail.com';
         });
     },
     
